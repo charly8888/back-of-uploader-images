f5e0073ca2bfb4b39b601b09f55d151fb3738db5
German Hornus <hornusgerman@gmail.com>
2022-06-06 16:16:01 -0300

    antes del multer

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 console.clear()
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-
 dotenv.config()
 
-const port = process.env.PORT || 4445
-
+const port = process.env.PORT || 3003
 const expressApp = express()
 
-expressApp.get('/image', (req, res) => {
+expressApp.use(express.static('public/images'))
+expressApp.use(express.static('public'))
+expressApp.use(express.json())
+expressApp.use(cors())
+
+expressApp.post('/', (req, res) => {
+  console.log(req.body)
   res.send('recibido')
 })
 
+expressApp.get('/imagen', (req, res) => {
+
+  res.send(`http://localhost:${port}/image.jpeg`)
+})
 expressApp.listen(port, () => {
   console.log('hola desde el puerto ' + port)
 })
