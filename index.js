92d59cb4cf920636b83d7ea3e693510b3f5b4360
German Hornus <hornusgerman@gmail.com>
2022-06-07 13:22:49 -0300

    back beta version 1.0

--- index.js
+++ index.js
@@ -2,25 +2,40 @@ console.clear()
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import fs from 'fs'
+import multer from 'multer'
 dotenv.config()
 
+const upload = multer({ storage: multer.memoryStorage() })
 const port = process.env.PORT || 3003
 const expressApp = express()
 
-expressApp.use(express.static('public/images'))
-expressApp.use(express.static('public'))
-expressApp.use(express.json())
+expressApp.use(express.static('uploads'))
 expressApp.use(cors())
+expressApp.use(express.json())
 
-expressApp.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('recibido')
+expressApp.post('/', upload.single('file'), (req, res) => {
+  const file = req.file
+  console.log(file)
+
+  const type = function (mimetype) {
+    let arr = mimetype.split('/')
+    console.log(arr)
+    return arr
+  }
+  if (type(file.mimetype)[0] !== 'image')
+    return res.status(400).send('el archivo no es de tipo imagen')
+
+  const fileName = +new Date()
+  const typeOfFile = type(file.mimetype)[1]
+  const newPath = `uploads/${fileName}.${typeOfFile}`
+  fs.writeFileSync(newPath, req.file.buffer)
+
+  res.send({
+    newPath: `http://localhost:${port}/${fileName}.${typeOfFile}`,
+  })
 })
 
-expressApp.get('/imagen', (req, res) => {
-
-  res.send(`http://localhost:${port}/image.jpeg`)
-})
 expressApp.listen(port, () => {
   console.log('hola desde el puerto ' + port)
 })
